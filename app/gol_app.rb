942f4544d8cd377f0c2642a66b0fa59d47590c56
Nadia Salmen <nadiasalmen@gmail.com>
2022-08-02 16:29:40 +0200

    Add GOL app

---
+++ app/gol_app.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative 'game_controller'
+require_relative 'validate_text_file_service'
+
+path_to_file = ARGV[0]
+validation_errors = ValidateTextFileService.new(path_to_file).call
+
+unless validation_errors.empty?
+  puts 'Your file is invalid. Please, check the following errors:'
+  validation_errors.each do |error|
+    puts error
+  end
+  puts 'Please, try again. Your .txt file should match the following format:'
+  puts '-------------------------------------------'
+  puts 'Generation 5:
+  4 8
+  ........
+  ...**...
+  ...**...
+  ........'
+  puts '-------------------------------------------'
+  puts 'Exiting...'
+  exit
+end
+
+puts '-------------------------------------------'
+puts 'Welcome to the Game of Life!'
+puts '-------------------------------------------'
+
+File.open('lib/initial_generation.txt', 'w') do |file|
+  file.write(File.read(path_to_file))
+end
+
+controller = GameController.new
+controller.show_next_generation
